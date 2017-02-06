ae0ed3fcb0ba180e8bc09de1c15a1f80f925b994
Carsten Haubold <carstenhaubold@googlemail.com>
2017-02-06 15:15:20 -0800

    inference tool: Fix soft constraints - solver must be created after adding the constraints to the model.

--- tools/inference.cpp
+++ tools/inference.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstdlib>
 #include <chrono>
 #include "pgmlink/pgm.h"
 #include "pgmlink/inferencemodel/constraint_pool.hxx"
 #include <opengm/graphicalmodel/graphicalmodel_hdf5.hxx>
 #include <opengm/inference/icm.hxx>
 #include <opengm/inference/lazyflipper.hxx>
+
+typedef opengm::Adder OperatorType;
+
+template<typename ITERATOR>
+double countNumViolatedSoftConstraints(pgmlink::PertGmType& model, ITERATOR labels)
+{
+    size_t numViolated = 0;
+    std::vector<size_t> factor_state(model.factorOrder()+1);
+    for(size_t j = 0; j < model.numberOfFactors(); ++j)
+    {
+        // skip if this is not a constraint function!
+        // (function index determined by counting in the function type list of PertGm)
+        if(model[j].functionType() > 10)
+            continue;
+
+        factor_state[0]=0;
+        // construct labeling
+        for(size_t i = 0; i < model.numberOfVariables(j); ++i)
+        {
+            factor_state[i] = labels[model.variableOfFactor(j, i)];
+        }
+
+        // evaluate function
+        if(model[j](factor_state.begin()) > 0)
+            numViolated += 1;
+    }
+    return numViolated;
+}
 
 int main(int argc, char** argv)
 {
@@ -136,28 +165,61 @@ int main(int argc, char** argv)
     }
     else if(inference_type == "ICM")
     {
-        // configure ICM
-        opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf(model);
+        size_t numRuns = 10;
+        std::srand(std::time(0));
+        solution_value = std::numeric_limits<double>::max();
+        opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> infDummy(model);
         cp.set_big_m(big_m);
-        cp.add_constraints_to_problem(model, inf);
+        cp.add_constraints_to_problem(model, infDummy);
+        std::cout << "After inserting soft constraints, model has " << model.numberOfFactors() << " factors" << std::endl;
 
-        // run inference
-        opengm::InferenceTermination status = inf.infer();
-
-        if (status != opengm::NORMAL)
+        for(size_t run = 0; run < numRuns; run++)
         {
-            throw std::runtime_error("ICM optimizer terminated abnormally");
+            std::vector<size_t> currentSolution;
+            double currentSolutionValue;
+
+            // set up a random initialization
+            std::vector<size_t> randomInit(model.numberOfVariables(), 0);
+            if(run > 0)
+            {
+                for(size_t v = 0; v < randomInit.size(); v++)
+                {
+                    randomInit[v] = std::rand() % model.numberOfLabels(v);
+                }
+            }
+
+            // configure ICM
+            opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf(model, randomInit);
+
+
+            // run inference
+            opengm::InferenceTermination status = inf.infer();
+
+            if (status != opengm::NORMAL)
+            {
+                throw std::runtime_error("ICM optimizer terminated abnormally");
+            }
+
+            // extract and print solution
+            status = inf.arg(currentSolution);
+
+            if (status != opengm::NORMAL)
+            {
+                throw std::runtime_error("Could not extract solution from ICM");
+            }
+
+            currentSolutionValue = inf.value();
+
+            std::cout << "Found solution with value " << currentSolutionValue << " and "
+                      << countNumViolatedSoftConstraints(model, currentSolution) << " violated constraints" << std::endl;
+
+            if(solution_value > currentSolutionValue)
+            {
+                solution_value = currentSolutionValue;
+                solution.clear();
+                solution.insert(solution.begin(), currentSolution.begin(), currentSolution.end());
+            }
         }
-
-        // extract and print solution
-        status = inf.arg(solution);
-
-        if (status != opengm::NORMAL)
-        {
-            throw std::runtime_error("Could not extract solution from ICM");
-        }
-
-        solution_value = inf.value();
     }
     else if (inference_type == "LP+ICM")
     {
@@ -186,9 +248,10 @@ int main(int argc, char** argv)
         std::cout << "Value of LP solution: " << inf.value() << std::endl;
 
         opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator>::Parameter param2(solution);
-        opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf2(model, param2);
+        opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> infDummy(model, param2);
         cp.set_big_m(big_m);
-        cp.add_constraints_to_problem(model, inf2);
+        cp.add_constraints_to_problem(model, infDummy);
+        opengm::ICM<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf2(model, param2);
 
         status = inf2.infer();
         if (status != opengm::NORMAL)
@@ -208,9 +271,10 @@ int main(int argc, char** argv)
     else if(inference_type == "LazyFlip")
     {
         // configure Lazyflip
-        opengm::LazyFlipper<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf(model);
+        opengm::LazyFlipper<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> infDummy(model);
         cp.set_big_m(big_m);
-        cp.add_constraints_to_problem(model, inf);
+        cp.add_constraints_to_problem(model, infDummy);
+        opengm::LazyFlipper<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf(model);
 
         // run inference
         opengm::InferenceTermination status = inf.infer();
@@ -237,24 +301,25 @@ int main(int argc, char** argv)
 
     std::cout << "\n===============================\nFound Solution:" << std::endl;
 
-    for(auto it = solution.begin(); it != solution.end(); ++it)
-    {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
+//    for(auto it = solution.begin(); it != solution.end(); ++it)
+//    {
+//        std::cout << *it << " ";
+//    }
+//    std::cout << std::endl;
 
     std::cout << "Solution has value: " << solution_value << std::endl;
 
     if(inference_type == "CPLEX" || inference_type == "LP")
     {
         std::cout << "Adding constraints" << std::endl;
-        opengm::LazyFlipper<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> inf(model);
+        opengm::LazyFlipper<pgmlink::PertGmType, pgmlink::pgm::OpengmModelDeprecated::ogmAccumulator> infDummy(model);
         cp.set_big_m(big_m);
-        cp.add_constraints_to_problem(model, inf);
+        cp.add_constraints_to_problem(model, infDummy);
     }
 
     evaluate_value = model.evaluate(solution);
     std::cout << "Evaluating the model with that solution: " << evaluate_value << std::endl;
+    std::cout << "Number of violated soft constraints: " << countNumViolatedSoftConstraints(model, solution) << std::endl;
 
     return 0;
 }
